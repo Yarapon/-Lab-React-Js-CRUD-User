8aca54af7c6ce8e38986f3ae48694f8a7a7374f6
YourGitHubUsername <YourEmail@example.com>
2025-03-17 04:03:33 +0700

    update project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Users from './Users';
+import Navbar from './Navbar';
+import { Routes, Route } from 'react-router-dom';
+import AddUser from './AddUser';
+import UserUpdate from './UserUpdate';
+import UserDelete from "./UserDelete"; // Import UserDelete Component
+
+export default function App() {
+  return (
+    <div> 
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Users />} />
+        <Route path="/create" element={<AddUser />} />
+        <Route path="/update/:id" element={<UserUpdate />} />
+        <Route path="/delete/:id" element={<UserDelete />} /> {/* เชื่อม UserDelete */}
+      </Routes>
+    </div>
+  )
+
+}
